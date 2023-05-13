8b44375b4541eae583b38a3119c57c0ab84af823
tomebarros <tomeobarros@gmail.com>
2023-05-13 15:55:55 +0800

    ubah nav link

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
     <ul class="navbar-link">
       <li><a href="#home">Home</a></li>
       <li><a href="#about">About</a></li>
-      <li><a href="#services">Service</a></li>
+      <li><a href="#services">Services</a></li>
       <li><a href="pasien/userinterface/">login</a></li>
     </ul>
 
