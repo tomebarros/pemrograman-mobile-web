2685a226213bfa3831030fd5e708466c97007aaf
tomebarros <tomeobarros@gmail.com>
2023-05-24 22:23:28 +0800

    menambahkan pesan balasan status Pesan

--- admin/userinterface/isipesan.php
+++ admin/userinterface/isipesan.php
@@ -28,7 +28,7 @@ $dataPesan = query("SELECT * FROM pesan WHERE idpesan = '$idpesan'")[0];
           <li>Email : <?= $dataPesan['email']; ?></li>
           <li>Judul : <?= $dataPesan['judul']; ?></li>
           <li>Tanggal : <?= tanggal($dataPesan['tanggal']); ?></li>
-          <li>Status : <?= $dataPesan['status']; ?></li>
+          <li>Status : <?= $dataPesan['status'] == '0' ? 'Belum Dibaca' : 'Dibaca'; ?></li>
         </ul>
         <h4><?= $dataPesan['judul']; ?></h4>
         <p><?= $dataPesan['isipesan']; ?></p>
@@ -54,3 +54,41 @@ $dataPesan = query("SELECT * FROM pesan WHERE idpesan = '$idpesan'")[0];
 </body>
 
 </html>
+
+
+<?php
+if ($dataPesan['status'] == '0') {
+  mysqli_query($koneksi, "UPDATE pesan SET status = '1' WHERE idpesan = '$idpesan'");
+
+
+  ini_set('display_errors', 1);
+  error_reporting(E_ALL);
+
+  // Required variables
+  $FROMEMAIL  = '"TomeHealth" <cs@21120055rekammedis.my.id>';
+  $TOEMAIL    = $dataPesan['email'];
+  $SUBJECT    = "Status Pesan Dari CS TomeHealth";
+  $PLAINTEXT  = "Salam Hangat, Tim 21120055rekammedis.my.id";
+  $RANDOMHASH = "anyrandomhash";
+  $FICTIONALSERVER = "@21120055lrekammedis.my.id";
+
+
+  // Basic headers
+  $headers = "From: " . $FROMEMAIL . "\n";
+  $headers .= "Reply-To: " . $FROMEMAIL . "\n";
+  $headers .= "Return-path: " . $FROMEMAIL . "\n";
+  $headers .= "Message-ID: <" . $RANDOMHASH . $FICTIONALSERVER . ">\n";
+  $headers .= "Dear {$dataPesan['nama']}, \n";
+  $headers .= "Pesan Anda Yang Berjudul '{$dataPesan['judul']}' telah dibaca oleh TIM 21120055rekammedis.my.id . \n";
+  $headers .= "Status Pesan : Dibaca. \n";
+
+  // Convert plain text body to quoted printable
+  $message = quoted_printable_encode($PLAINTEXT);
+
+  // Create a BASE64 encoded subject
+  $subject = "=?UTF-8?B?" . base64_encode($SUBJECT) . "?=";
+
+  // Send email
+  mail($TOEMAIL, $subject, $message, $headers, "-f" . $FROMEMAIL);
+}
+?>
