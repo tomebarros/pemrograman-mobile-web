ae7b35dd8ca38bd7e0e8153fd5c4b806801057fe
tomebarros <tomeobarros@gmail.com>
2023-06-08 10:01:53 +0800

    toggle dark mode

--- pasien/aset/navbar.php
+++ pasien/aset/navbar.php
@@ -13,12 +13,26 @@ $nama = namapasien($_SESSION['emailPasien']);
       <ul class="navbar-nav ms-auto">
 
         <li class="nav-item">
+          <div class="nav-link dropdown">
+            <button class="btn dropdown-toggle btn-sm " type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+              <i class="bi bi-circle-half" id="theme"></i>
+            </button>
+            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton" data-bs-dropdown>
+              <li><a class="dropdown-item" href="#" id="dark"><i class="bi bi-moon-stars-fill me-2"></i> Dark</a></li>
+              <li><a class="dropdown-item" href="#" id="light"><i class="bi bi-sun-fill me-2"></i> Light</a></li>
+            </ul>
+          </div>
+        </li>
+
+
+        <li class="nav-item">
           <a class="nav-link active" href="riwayatrekammedis.php">Rekam Medis</a>
         </li>
 
         <li class="nav-item">
           <a class="nav-link active" href="#" data-bs-toggle="modal" data-bs-target="#ModalProfil"><i class="bi bi-person-fill"></i><?= $nama; ?></a>
         </li>
+
 
         <li class="nav-item">
           <a class="nav-link" href="../controller/other/ceklogout.php">Logout</a>
--- pasien/aset/navbarlogin.php
+++ pasien/aset/navbarlogin.php
@@ -7,17 +7,6 @@
     <div class="collapse navbar-collapse" id="navbarNav">
 
       <ul class="navbar-nav ms-auto">
-        <li class="nav-item">
-          <div class="nav-link dropdown">
-            <button class="btn dropdown-toggle btn-sm " type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
-              <i class="bi bi-circle-half" id="theme"></i>
-            </button>
-            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton" data-bs-dropdown>
-              <li><a class="dropdown-item" href="#" id="dark"><i class="bi bi-moon-stars-fill me-2"></i> Dark</a></li>
-              <li><a class="dropdown-item" href="#" id="light"><i class="bi bi-sun-fill me-2"></i> Light</a></li>
-            </ul>
-          </div>
-        </li>
 
         <li class="nav-item">
           <a href="pendaftaranbaru.php" class="nav-link active">Pendaftaran Pasienbaru</a>
