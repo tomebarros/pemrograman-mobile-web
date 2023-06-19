47191cf11a3d4f1668fb26fdd922f4535e44d8df
tomebarros <tomeobarros@gmail.com>
2023-06-20 01:17:47 +0800

    pagination disabled

--- template/pagination.php
+++ template/pagination.php
@@ -2,8 +2,9 @@
 
   <?php if ($halamanAktif > 1) { ?>
     <a href="?page=<?= $halamanAktif - 1; ?>"><i class="fa-solid fa-angle-left"></i></a>
+  <?php } else { ?>
+    <a href="#" class="disabled"><i class="fa-solid fa-angle-left"></i></a>
   <?php } ?>
-
 
   <?php for ($i = 1; $i <= $jumlahHalaman; $i++) { ?>
     <?php if ($i == $halamanAktif) { ?>
@@ -16,8 +17,9 @@
 
   <?php if ($halamanAktif < $jumlahHalaman) { ?>
     <a href="?page=<?= $halamanAktif + 1 ?>"><i class="fa-solid fa-angle-right"></i></a>
+  <?php } else { ?>
+    <a href="#" class="disabled"><i class="fa-solid fa-angle-right"></i></a>
   <?php } ?>
-
 
 
 
