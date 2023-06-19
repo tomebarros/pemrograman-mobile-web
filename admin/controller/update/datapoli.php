6a61c7e4257c7d7bd8192fc993813c14f6165312
tomebarros <tomeobarros@gmail.com>
2023-06-20 00:07:51 +0800

    duplikasi poli ubah

--- admin/controller/update/datapoli.php
+++ admin/controller/update/datapoli.php
@@ -1,14 +1,20 @@
 <?php
 include '../../../template/functions.php';
-
-
+include '../other/restrict.php';
 
 // menangkap data yang di kirim dari form
-$namaVariabel1 = input($_POST['namapoli']);
-$namaVariabel = input($_POST['idpoli']);
+$namaPoli = input($_POST['namapoli']);
+$idpoli = input($_POST['idpoli']);
 
-// update data ke database
-mysqli_query($koneksi, "update poli set namapoli='$namaVariabel1' where idpoli='$namaVariabel'");
+$duplikasi = getData("SELECT * FROM poli WHERE namapoli LIKE '$namaPoli' AND idpoli != '$idpoli'");
 
-// mengalihkan halaman kembali ke index.php
-header("location: ../../userinterface/datapoli.php");
+if ($duplikasi > 0) {
+  echo "<script>
+    alert('Data Sudah Tersedia');
+    history.go(-1);
+  </script>";
+  die;
+} else {
+  mysqli_query($koneksi, "update poli set namapoli='$namaPoli' where idpoli='$idpoli'");
+  header("location: ../../userinterface/datapoli.php");
+}
