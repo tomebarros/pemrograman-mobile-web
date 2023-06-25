de73f7bbbaea682ecf61cf9954d85bcd961880ce
tomebarros <tomeobarros@gmail.com>
2023-06-25 14:17:52 +0800

    profile dinamis

--- admin/aset/header.php
+++ admin/aset/header.php
@@ -1,5 +1,16 @@
+<?php
+$nama = explode(' ', $dataAdmin['nama']);
+$nama = $nama[0];
+?>
 <header>
-  <p>Admin</p>
-  <img src="../aset/img/profile/user.png" alt="Profile">
+  <p><?= $nama; ?></p>
+  <?php
+  $namaFile = $dataAdmin['idkaryawan'];
+  if (file_exists("../aset/img/profile/$namaFile.png")) { ?>
+    <img src="../aset/img/profile/<?= $namaFile; ?>.png" alt="Profile">
+  <?php } else { ?>
+    <img src="../aset/img/profile/user.png" alt="Profile">
+  <?php } ?>
+
   <i class="fa-solid fa-bars" id="toggleNavbar"></i>
 </header>
