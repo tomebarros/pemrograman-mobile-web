87381233412a64a0c80a245719bd5377c0b7d712
tomebarros <tomeobarros@gmail.com>
2023-06-27 00:34:53 +0800

    perbaiki form input

--- pasien/userinterface/index.php
+++ pasien/userinterface/index.php
@@ -15,6 +15,7 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 
   <link rel="stylesheet" href="../aset/css/style.css" />
+  <link rel="shortcut icon" href="../../img/logo.png" type="image/x-icon">
 </head>
 
 <body class="da">
